6f6daefe7d40f9351981844ad0ab78d228bd311b
Irina <monchenko.i.d@gmail.com>
2017-10-16 21:36:51 +0300

    Update ICalculator.java

--- app/src/main/java/com/github/a5809909/hwork02_testsimplecalculator/ICalculator.java
+++ app/src/main/java/com/github/a5809909/hwork02_testsimplecalculator/ICalculator.java
@@ -1,5 +1,5 @@
 package com.github.a5809909.hwork02_testsimplecalculator;
-
+//TODO add new line between methods
 public interface ICalculator {
     String addition(String num1, String num2);
     String subtraction(String num1, String num2);
